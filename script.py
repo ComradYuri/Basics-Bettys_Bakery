251fe3eaf01f269dbff4bfc3ca7f27ab7d375c1e
Joerie <joerie_svd@hotmail.com>
2019-02-25 13:23:25 +0100

    completed exercise with comments

--- script.py
+++ script.py
@@ -1,1 +1,24 @@
 import numpy as np
+
+cupcakes = np.array([2, 0.75, 2, 1, 0.5])
+print('cupcakes\n', cupcakes)
+
+recipes = np.genfromtxt('recipes.csv', delimiter=',')
+
+# columns: Cups of Flour, Cups of sugar, Eggs, Cups of milk, Cups of butter
+# rows: Cupcakes, Pancake, Cookie, Bread
+print('recipes\n', recipes)
+
+eggs = recipes[:, 2]
+print('eggs required per recipe\n', eggs)
+
+print('recipes that require one egg\n', eggs == 1)
+
+cookies = recipes[2, :]
+print('requirements for cookie recipe\n', cookies)
+
+double_batch = cupcakes * 2
+print('double pancake batch requirements\n', double_batch)
+
+grocery_list = cookies + double_batch
+print('grocery list for cookie and two pancakes\n', grocery_list)
